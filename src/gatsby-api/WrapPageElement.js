2e79b9a3759277262d2253ac5b34a8426a37aa04
Matthew Pagan <mastapegs01@gmail.com>
2020-12-05 12:38:14 -0500

    refactor wrappagelement api

--- src/gatsby-api/WrapPageElement.js
+++ src/gatsby-api/WrapPageElement.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-const WrapPageElement = ({ element, props }) => {
-  return (
-    <>
-      <Layout {...props}>{element}</Layout>
-    </>
-  )
-}
+const WrapPageElement = ({ element, props }) => <Layout {...props}>{element}</Layout>
+
 export default WrapPageElement
