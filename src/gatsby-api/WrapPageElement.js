206c30a177052282751e9301a8a4a61e5d7318ed
Matthew Pagan <mastapegs01@gmail.com>
2020-12-05 12:12:21 -0500

    create layout component w/ gatsby api

---
+++ src/gatsby-api/WrapPageElement.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const WrapPageElement = ({ element, props }) => {
+  return (
+    <>
+      <Layout {...props}>{element}</Layout>
+    </>
+  )
+}
+export default WrapPageElement
